39b3c04cfe852e9ef3cfd2841cdb30132d9b954b
Vitor Ricardo <vitorricardo@outlook.com>
2025-03-23 22:50:04 -0300

    chore: rename buckets localstack

--- localstack/docker-entrypoint.sh
+++ localstack/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create bucket S3
-aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket ps_private
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket bucket_private
 
-aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket ps_public
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket bucket_public
 
 echo 'Localstack configurado.'
